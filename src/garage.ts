5f456ca46a6b10c0bd0cc49343e8cdd2a592665b
Lights0n <lyadouskiy@gmail.com>
2023-01-29 00:31:40 +0300

    feat: add form to create cars

---
+++ src/garage.ts
@@ -0,0 +1,7 @@
+import { colorType } from "./types";
+class Garage {
+  protected color: colorType;
+  constructor(color: colorType,) {
+    this.color = color;
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,2 @@
+// const colortype = new RegExp(`/^#([0-9a-f]{3}|[0-9a-f]{6})$/i`);
+export type colorType = `#${string}`;
